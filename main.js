afc29ce06c9a416e95e349bb639bb6a7bae1f22f
NataliaNazarenko <115888204+NataliaNazarenko@users.noreply.github.com>
2023-12-21 22:45:32 +0200

    add f generateTetromino() and drowTetromino()

--- main.js
+++ main.js
@@ -10,11 +10,54 @@ const TETROMINOES = {
     ]
 };
 
+let playfield;
+let tetromino;
+
+function convertPositionToIndex(row, column) {
+    return row * PLAYFIELD_COLUMNS + column;
+};
+
 function generatePlayfield() {
     for(let i = 0; i < PLAYFIELD_ROWS * PLAYFIELD_COLUMNS; i+=1) {
         const div = document.createElement('div');
         document.querySelector('.tetris').append(div);
-    }
+    };
+
+    playfield = new Array(PLAYFIELD_ROWS).fill().map(() => new Array(PLAYFIELD_COLUMNS).fill(0));
+    // console.log(playfield);
 };
 
+function generateTetromino() {
+    const nameTetro = "O";
+    const matrixTetro = TETROMINOES[nameTetro];
+
+    const rowTetro = 3;
+    const columnTetro = 5;
+
+    tetromino = {
+        name: nameTetro,
+        matrix: matrixTetro,
+        row: rowTetro,
+        column: columnTetro,
+    };
+}
+
 generatePlayfield();
+generateTetromino();
+
+const cells = document.querySelectorAll('.tetris div');
+console.log(cells);
+
+function drowTetromino() {
+    const name = tetromino.name;
+    const tetrominoMatrixSize = tetromino.matrix.length;
+
+    for(let row = 0; row < tetrominoMatrixSize; row +=1) {
+        for(let column = 0; column < tetrominoMatrixSize; column +=1) {
+            const cellIndex = convertPositionToIndex(tetromino.row + row, tetromino.column + column);
+            cells[cellIndex].classList.add(name);
+        };
+    };
+};
+
+drowTetromino();
